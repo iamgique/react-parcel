167374ed5ccdf86bfaf670b17afed04c82931878
Ayuth Mangmesap <ayuth.mang@gmail.com>
2019-09-17 14:34:55 +0700

    update babel, make app support class properties

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,27 @@ import { Alert, Badge } from 'reactstrap';
 import '../styles/style.scss'
 import logo from '../../assets/images/react.png'
 
-const App = () => (
-  <div className="App">
-    <Alert color="primary">
-      This is a demo react with <a href="https://parceljs.org/" className="alert-link">parceljs</a>.
-      Using module bundler to prove whether is it faster and easier than create-react-app or webpack?
+class App extends React.Component {
+  state = {
+    message: 'Hello World!'
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Alert color="primary">
+          This is a demo react with <a href="https://parceljs.org/" className="alert-link">parceljs</a>.
+          Using module bundler to prove whether is it faster and easier than create-react-app or webpack?
         <Badge color="secondary">Import reactstrap (bootstrap)</Badge>
-    </Alert>
-    <div className="nav-style">
-      Import by own style
-    </div>
-    <img src={logo} alt="React ICON" />
-  </div>
-)
+        </Alert>
+        <div className="nav-style">
+          {this.state.message}
+        </div>
+        <img src={logo} alt="React ICON" />
+      </div>
+    )
+  };
+
+}
 
 export default App
